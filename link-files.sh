27deaa4e5cf89c439aaaa9a57605317ed48d9392
thejsj <jorge.silva@thejsj.com>
2015-11-23 20:48:56 -0800

    Fixing script to link dotfiles

--- link-files.sh
+++ link-files.sh
@@ -1,12 +1,58 @@
 #!/bin/bash
-for file in ./.*
+
+# Copy Dot Files that are not git files
+for file in $(find . -maxdepth 1)
 do
-	if  [[ $file != ./.git* ]] && [[ $FILE != *.  ]] && [[ -f $file || -d $file ]];
+	if  [[ $file != ./.git* ]] && [[ $file != *.  ]];
 	then
 		FILE_NAME=$(basename "$file")
 		CURRENT_DIR=$(pwd)
 		SOURCE_PATH="$CURRENT_DIR/$FILE_NAME"
 		TARGET_PATH="$CURRENT_DIR/../$FILE_NAME"
-		ln -s $SOURCE_PATH $TARGET_PATH
-	fi
+
+
+		if ! [[ -L $TARGET_PATH ]];
+		then
+
+			# File
+			if [[ -f $SOURCE_PATH ]];
+			then
+				echo -n "File ($TARGET_PATH) already exists. Do you want to delete it? (Y[es]/N[o]) "
+				if read -t 60 DELETE_EXISTING_FILE;
+				then
+					if [[ $DELETE_EXISTING_FILE = y* || $DELETE_EXISTING_FILE == Y* ]];
+					then
+						rm $TARGET_PATH
+						ln -s $SOURCE_PATH $TARGET_PATH
+					else
+						echo "File not linked. ($TARGET_PATH)"
+					fi;
+				else
+					echo "File not linked. ($TARGET_PATH)"
+				fi;
+			else
+				ln -s $SOURCE_PATH $TARGET_PATH
+			fi
+
+			# Directory
+			if [[ -d $SOURCE_PATH ]];
+			then
+				echo -n "Directory ($TARGET_PATH) already exists. Do you want to delete it (rm -rf)? (Y[es]/N[o]) "
+				if read -t 60 DELETE_EXISTING_FILE;
+				then
+					if [[ $DELETE_EXISTING_FILE = y* || $DELETE_EXISTING_FILE == Y* ]];
+					then
+						rm -rf $TARGET_PATH
+						ln -s $SOURCE_PATH $TARGET_PATH
+					else
+						echo "File not linked. ($TARGET_PATH)"
+					fi;
+				else
+					echo "File not linked. ($TARGET_PATH)"
+				fi;
+			else
+				ln -s $SOURCE_PATH $TARGET_PATH
+			fi;
+		fi;
+	fi;
 done
